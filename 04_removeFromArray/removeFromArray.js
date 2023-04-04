bf3cad843941fbf1109412d75f0b65d3ec000e0c
Akarsh <akarsh.bangalore@gmail.com>
2023-04-03 21:56:09 -0500

    Completed excercise 4 to remove elements from an array. All tests are passing.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...theArgs) {
+    return array.filter(x => !theArgs.includes(x))
 };
 
 // Do not edit below this line
