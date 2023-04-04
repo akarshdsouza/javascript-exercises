c993997c1dbb65829c7e893184b4cdc98e369b03
Akarsh <akarsh.bangalore@gmail.com>
2023-04-04 04:31:28 -0500

    Completed excercise 7 to convert temperature from celsius to fahrnheit and vice versa. All tests are passing

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celsius = (temp - 32) * 5 / 9;
+  const rounded = (value, precision) => {
+    let multiplier = Math.pow(10, precision || 0);
+    return Math.round(value * multiplier) / multiplier;
+  }
+  return rounded(celsius, 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fahrenheit = (temp * (9 / 5)) + 32;
+  const rounded = (value, precision) => {
+    let multiplier = Math.pow(10, precision || 0);
+    return Math.round(value * multiplier) / multiplier;
+  }
+  return rounded(fahrenheit, 1);
 };
 
 // Do not edit below this line
