70652743e06da02d964b1f3cbfbcb6c42e8782cc
Akarsh <akarsh.bangalore@gmail.com>
2023-04-09 00:57:40 -0500

    Completed excercise 8 to build a caluclator that performs different calculation. All tests are passing

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let total = 0;
+  array.forEach( x => {
+    total += x;
+  });
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let total = 1;
+  array.forEach( x => {
+    total *= x;
+  });
+  return total;
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+  let total = 1;
+	for (let i = 1; i <= power; i++) {
+    total *= base;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let fact = 1;
+  for(i = 1; i <= num; i++) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
