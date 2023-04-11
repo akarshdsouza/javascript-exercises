0b014dc292bda22d74de15c38c6cd84d6a2600db
Akarsh <akarsh.bangalore@gmail.com>
2023-04-11 02:48:16 -0500

    Completed excercise 9 for checking whether a given string is a palindrome or not. All tests are passing

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const finalString = str.toLowerCase().replace(/[^a-z]/g, "");
+    return finalString.split("").reverse().join("") == finalString;
 };
 
 // Do not edit below this line
