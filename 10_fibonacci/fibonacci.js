ccc5c672d1328e05dce4f6efcc484545b8fdcef5
Akarsh <akarsh.bangalore@gmail.com>
2023-04-24 22:03:01 -0500

    Completed excercise 10 to find the fibonacci number of the given decimal number. All tests are passing

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+    if (number < 0) return 'OOPS';
+    if (number == 0) return 0;
+    let num1 = 0;
+    let num2 = 1;
+    for (let i = 1; i < number; i++) {
+        const temp = num2;
+        num2 = num1 + num2;
+        num1 = temp;
+    }
+    return num2;
 };
 
 // Do not edit below this line
